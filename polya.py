bf67d82c056750e95b46b700c6433d4d6422a2fc
neal-o-r <neal@n-o-r.xyz>
2017-10-16 19:40:55 +0100

    means

--- polya.py
+++ polya.py
@@ -1,7 +1,18 @@
 import random as rd
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+import scipy.stats
 
 plt.style.use('ggplot')
+
+def mean_confidence_interval(data, confidence=0.95):
+	a = 1.0*np.array(data)
+	n = len(a)
+	m, se = np.mean(a), scipy.stats.sem(a)
+	h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
+	return m, h
+
 
 class Urn(object):
 
@@ -17,8 +28,12 @@ class Urn(object):
 
 
 dist = [rd.sample([0,0,0,1], 1)[0] for i in range(2000)]
+s = rd.sample(dist, 100)
+m, h = mean_confidence_interval(s)
 
 plt.axhline(sum(dist)/len(dist))
+plt.axhline(m+h, ls='--', c='r') 
+plt.axhline(m-h, ls='--', c='r') 
 
 for _ in range(50):
 
@@ -30,6 +45,7 @@ for _ in range(50):
                 urn_contents.append(sum(u.urn)/len(u.urn))
 
         plt.plot(urn_contents, color='0.7', alpha=0.3)
-        plt.ylim(0,1)
 
+plt.ylim(0,1)
+plt.xlim(0,len(urn_contents)-1)
 plt.show()
