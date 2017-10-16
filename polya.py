b5e7ce04f8e1dfc4c1e6565ff467c185a34e0350
Neal Ó Riain <neal@n-o-r.xyz>
2017-10-16 11:22:57 +0100

    Update polya.py

--- polya.py
+++ polya.py
@@ -1,25 +1,35 @@
 import random as rd
 import matplotlib.pyplot as plt
 
+plt.style.use('ggplot')
+
 class Urn(object):
 
-	def __init__(self, samples):
-		self.samples = samples
-		self.urn = samples
-	
-	def grow_sample(self, n):
-		
-		while len(self.urn) < n:
-			item = rd.sample(self.urn, 1)
-			self.urn += item
+        def __init__(self, samples):
+                self.samples = samples
+                self.urn = samples
+       
+        def grow_sample(self, n):
+               
+                while len(self.urn) < n:
+                        item = rd.sample(self.urn, 1)
+                        self.urn += item
 
 
 dist = [rd.sample([0,0,0,1], 1)[0] for i in range(2000)]
 
-u = Urn(rd.sample(dist, 100))
-u.grow_sample(2000)
+plt.axhline(sum(dist)/len(dist))
 
-plt.hist(dist, alpha=0.5, label='Original')
-plt.hist(u.urn, alpha=0.5, label='Urn')
-plt.legend()
+for _ in range(50):
+
+        u = Urn(rd.sample(dist, 100))
+
+        urn_contents = []
+        for n in [i * 100 for i in range(2,20)]:
+                u.grow_sample(n)
+                urn_contents.append(sum(u.urn)/len(u.urn))
+
+        plt.plot(urn_contents, color='0.7', alpha=0.3)
+        plt.ylim(0,1)
+
 plt.show()
